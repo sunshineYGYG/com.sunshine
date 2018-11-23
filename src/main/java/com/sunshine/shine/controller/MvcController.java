0ddc2532d352f17e79e0b38f42d57bae9a988cd1
sunshine <1123754943@qq.com>
2018-11-23 12:35:56 +0800

    删除注释

---
+++ src/main/java/com/sunshine/shine/controller/MvcController.java
@@ -0,0 +1,29 @@
+package com.sunshine.shine.controller;
+
+
+import com.sunshine.shine.Util.JsonData;
+import com.sunshine.shine.dao.UserMapper;
+import com.sunshine.shine.dao.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
+@Controller
+public class MvcController {
+
+    @Resource
+    private UserMapper userMapper;
+
+    @ResponseBody
+    @GetMapping("/test/requestHeader")
+    public JsonData getUser(@RequestHeader Integer id){
+        User user = userMapper.selectOneUser(id);
+        return JsonData.success(user);
+    }
+
+}
